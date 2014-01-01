33be5adae564341f3a30a7ed967228ea2bd19a1e
rgan <rajaram.ganeshan@gmail.com>
2014-01-01 15:32:10 -0500

    Return form data with post

--- formbuilder.py
+++ formbuilder.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def get_or_post(form_id, user_id):
     if request.method == 'POST':
         Answer.update(form_id, user_id, json.loads(request.data))
-        return json.dumps({"result" : True})
+        return request.data
     else:
         form = Form.from_id(form_id)
         form.update_with_answers(user_id)
